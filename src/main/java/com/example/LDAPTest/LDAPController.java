3f4bdf2c19a39ac1f56400fad7dfdecb32df5f2b
Ng Jun Hua <santa_crow@hotmail.com>
2021-03-23 14:22:53 +0800

    changed

--- src/main/java/com/example/LDAPTest/LDAPController.java
+++ src/main/java/com/example/LDAPTest/LDAPController.java
@@ -2,6 +2,7 @@ package com.example.LDAPTest;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.LdapTemplate;
 import static org.springframework.ldap.query.LdapQueryBuilder.query;
